ada07451d4f8ad1c43d4910685c7ae5b07b8ff41
kcNeko <kriziacamilleuayan@gmail.com>
2019-07-20 23:35:09 +0800

    redesign sign in

--- src/App.js
+++ src/App.js
@@ -1,36 +1,54 @@
-import React, {Component} from 'react';
-import {Router, Stack, Scene, ActionConst} from 'react-native-router-flux';
+import React, { Component } from "react";
+import { Router, Stack, Scene, ActionConst } from "react-native-router-flux";
 
-import Splash from './startup/Splash';
-import SignIn from './startup/SignIn'
-import ScanProduct from './product/ScanProduct';
-import ProductDetailsNormal from './product/ProductDetailsNormal';
-import ProductDetailsWarning from './product/ProductDetailsWarning';
-import Cart from './cart/Cart';
+import Splash from "./startup/Splash";
+import SignIn from "./startup/SignIn";
+import ScanProduct from "./product/ScanProduct";
+import ProductDetailsNormal from "./product/ProductDetailsNormal";
+import ProductDetailsWarning from "./product/ProductDetailsWarning";
+import Cart from "./cart/Cart";
 
 const RootStack = () => (
-    <Router>
-        <Stack key="root" hideNavBar={true}>
-            <Scene key="splash" component={Splash} hideNavBar={true} type={ActionConst.RESET} />
-            <Scene key="signIn" component={SignIn} hideNavBar={true} type={ActionConst.RESET} />
+  <Router>
+    <Stack key="root" hideNavBar={true}>
+      <Scene
+        key="splash"
+        component={Splash}
+        hideNavBar={true}
+        type={ActionConst.RESET}
+      />
+      <Scene
+        key="signIn"
+        component={SignIn}
+        hideNavBar={true}
+        type={ActionConst.RESET}
+      />
 
-            <Scene key="scanProduct" component={ScanProduct} hideNavBar={true} />
-            <Scene key="productNormal" component={ProductDetailsNormal} hideNavBar={true} />
-            <Scene key="productWarning" component={ProductDetailsWarning} hideNavBar={true} />
-            <Scene key="cart" component={Cart} hideNavBar={true} />
-        </Stack>
-    </Router>
+      <Scene key="scanProduct" component={ScanProduct} hideNavBar={true} />
+      <Scene
+        key="productNormal"
+        component={ProductDetailsNormal}
+        hideNavBar={true}
+      />
+      <Scene
+        key="productWarning"
+        component={ProductDetailsWarning}
+        hideNavBar={true}
+      />
+      <Scene key="cart" component={Cart} hideNavBar={true} />
+    </Stack>
+  </Router>
 );
 
-export default class App extends Component{
-    render(){
-        return(
-            // <Splash />
-            // <ScanProduct />
-            // <ProductDetailsNormal/>
-            // <ProductDetailsWarning/>
-            // <Cart/>
-            <RootStack/>
-        )
-    }
+export default class App extends Component {
+  render() {
+    return (
+      // <Splash />
+      // <ScanProduct />
+      // <ProductDetailsNormal/>
+      // <ProductDetailsWarning/>
+      // <Cart/>
+      <RootStack />
+    );
+  }
 }
--- src/startup/Splash.js
+++ src/startup/Splash.js
@@ -1,71 +1,68 @@
-import React, { Component } from 'react';
-import { View, StyleSheet, Dimensions, Image } from 'react-native'
-import { Container, Text } from 'native-base';
-import LinearGradient from 'react-native-linear-gradient';
-import {Actions} from 'react-native-router-flux';
-import logo from '../assets/easescan.png'
+import React, { Component } from "react";
+import { View, StyleSheet, Dimensions, Image } from "react-native";
+import { Container, Text } from "native-base";
+import LinearGradient from "react-native-linear-gradient";
+import { Actions } from "react-native-router-flux";
+import logo from "../assets/easescan.png";
 //import console = require('console');
 
 export default class Splash extends Component {
-    state = {
-        time: 3
+  state = {
+    time: 3
+  };
+  componentDidMount() {
+    console.log(window);
+    this.timerID = setInterval(() => this.countdown(), 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+    console.log("Closing splash page...");
+    Actions.signIn();
+  }
+  countdown() {
+    if (this.state.time > 0) {
+      this.setState({
+        time: this.state.time - 1
+      });
+      console.log(this.state.time);
+    } else {
+      this.componentWillUnmount();
     }
-    componentDidMount(){
-        console.log(window);
-        this.timerID = setInterval(
-            () => this.countdown(),
-            1000
-        );
-    }
-    componentWillUnmount(){
-        clearInterval(this.timerID)
-        console.log("Closing splash page...")
-        Actions.signIn();
-    }
-    countdown(){
-        if((this.state.time) > 0){
-            this.setState({
-                time: this.state.time-1
-            })
-            console.log(this.state.time); 
-        }
-        else{
-            this.componentWillUnmount()
-        }
-    }
-    render() {
-        return (
-            <Container>
-                <LinearGradient
-                    start={{ x: 0, y: 0 }}
-                    end={{ x: 0, y: 1.5 }}
-                    locations={[0, 0.5]}
-                    colors={["#ACB7AE", "#1DBAB4"]}
-                >
-                    <View style={styles.container}>
-                        <Image source={logo} style={styles.logo} />
-
-                    </View>
-
-
-                </LinearGradient>
-            </Container>
-        )
-    }
+  }
+  render() {
+    return (
+      <Container>
+        <LinearGradient
+          start={{ x: 0, y: 0 }}
+          end={{ x: 0, y: 1.5 }}
+          locations={[0, 0.5]}
+          colors={["#ACB7AE", "#1DBAB4"]}
+        >
+          <View style={styles.container}>
+            {/* <Image source={logo} style={styles.logo} /> */}
+            <Image
+              source={require('../assets/giphy.gif')}
+              style={{width: 100, height:100 }}
+            />
+          </View>
+        </LinearGradient>
+      </Container>
+    );
+  }
 }
 
-const height = Dimensions.get('window').height;
-const width = Dimensions.get('window').width;
+const height = Dimensions.get("window").height;
+const width = Dimensions.get("window").width;
 
 const styles = StyleSheet.create({
-    container: {
-        height: height,
-        width: width,
-        padding: 10
-    },
-    logo: {
-        resizeMode: 'contain',
-        alignSelf: 'center',
-        marginTop: '50%'
-    }
-})
+  container: {
+    height: height,
+    width: width,
+    padding: 10
+  },
+  logo: {
+    resizeMode: "contain",
+    alignSelf: "center",
+    marginTop: "50%"
+  }
+});
